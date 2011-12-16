3b6794c78da5011732b77c69dc64187326b73c0d
Charles Ha <cha@win.local>
2011-12-16 14:59:24 -0500

    can move the paddle now

--- scripts/src/paddle.js
+++ scripts/src/paddle.js
@@ -1,5 +1,25 @@
 $(function(){
   window.Paddle = Backbone.Model.extend({
+    defaults: {
+      width: 50,
+      left: 0,
+      bottom: 10
+    },
 
+    moveLeft: function () {
+      var x = this.get('left');
+      if (x > 0) {
+        x = x - 10;
+      }
+      this.set({'left': x});
+    },
+  
+    moveRight: function () {
+      var x = this.get('left');
+      if (x < 520) {
+        x = x + 10;
+      }
+      this.set({'left': x});
+    }
   });
 });
--- scripts/src/paddleView.js
+++ scripts/src/paddleView.js
@@ -1,9 +1,31 @@
 $(function(){
   window.PaddleView = Backbone.View.extend({
-    tagName: "div",
+    initialize: function () {
+      this.model.bind('change', this.render, this);
+    },
+    
+    tagName: "input",
 
+    id: 'paddle',
+
+    events: {
+      "keydown": "move",
+      "change": "render" 
+    },
+
+    move: function (e) {
+      if (e.keyCode === 37) {
+        this.model.moveLeft();
+      } else if (e.keyCode === 39){
+        this.model.moveRight();
+      }
+    },
+  
     render: function () {
-      $(this.el).attr('id', 'paddle');
+      $(this.el)
+        .css('bottom', this.model.get('bottom'))
+        .css('left', this.model.get('left'))
+        .css('width', this.model.get('width'));
       return this;
     }
   });
