f547ed1f79dee847acbc54dbcefddb2e51bddca9
Chuck Ha <ha.chuck@gmail.com>
2013-04-03 11:57:53 -0400

    Adds comments and makes a collision function

--- js/breakout.js
+++ js/breakout.js
@@ -1,8 +1,10 @@
 (function() {
+  // Engine setup
   var engine = window.engine = new glitz.Engine(document.getElementById('canvas'));
   engine.setSize($(window).width(), $(window).height()-5);
   engine.layout.backgroundColor = '#111';
 
+  // Models
   var Game = new glitz.Renderable({
     width: 365,
     height: 500,
@@ -44,6 +46,8 @@
   });
 
   var Paddle = new glitz.Renderable({
+    left: false,
+    right: false,
     width: 80,
     height: 10,
     render: function (ctx) {
@@ -54,11 +58,12 @@
     }
   });
 
+  // Game instance setup
   var game = new Game({x:300, y:10});
-
   var i, j;
   var brick;
   var bricks = [];
+  // FIXME: make a JSON object that can represent the layout of bricks for more than one level.
   for (j = 0; j < 8; j++) {
     for (i = 0; i < 6; i++) {
       brick = new Brick({x:5 + (5 + BPROPS.width) * j, y:5 + (5 + BPROPS.height) * i});
@@ -68,32 +73,56 @@
   }
 
   var ball = new Ball({x:game.width/2, y:game.height - 40});
-  game.push(ball);
   var paddle = new Paddle({x:0, y: game.height - 20})
+
+  game.push(ball);
   game.push(paddle);
   engine.push(game);
 
-  var paddleLeft = false;
-  var paddleRight = false;
-
+  // Set the key listening functions
+  // on each frame that paddle.<dir> is true, move the paddle <dir>
   $(document).keydown(function (e) {
     // l moves right
     if (e.which == 76) {
-      paddleRight = true;
+      paddle.right = true;
     }
     // h moves left
     if (e.which == 72) {
-      paddleLeft = true;
+      paddle.left = true;
     }
   }).keyup(function (e) {
     if (e.which == 76) {
-      paddleRight = false;
+      paddle.right = false;
     } else if (e.which == 72) {
-      paddleLeft = false;
+      paddle.left = false;
     }
   });
 
+
+  // Rect must have the following properties:
+  //     x, y, width, height
+  function point_rect_collision(x, y, rect) {
+    return (y <= rect.y + rect.height)
+      && (y >= rect.y)
+      && (x <= rect.x + rect.width)
+      && (x >= rect.x)
+  };
+
+  // Main game loop
   engine.loop( function() {
+
+    if (paddle.left) {
+      if (paddle.x > 0) {
+        paddle.x -= 5;
+      }
+    }
+    if (paddle.right) {
+      if (paddle.x + paddle.width < paddle.parent.width) {
+        paddle.x += 5;
+      }
+    }
+
+    // Define points on the ball
     var topx = ball.x;
     var topy = ball.y - ball.radius;
 
@@ -105,124 +134,74 @@
 
     var bottomx = ball.x;
     var bottomy = ball.y + ball.radius;
-    // paddle collision:
-    if (bottomy >= paddle.y) {
-      if (bottomy <= paddle.y + paddle.height) {
-        if (bottomx <= paddle.x + paddle.width) {
-          if (bottomx >= paddle.x) {
-            var normalized_distance_from_center = ((paddle.x + paddle.width/2) - ball.x) / paddle.width/2;
-            var reflected_angle = (Math.PI / 4) + 4 * (normalized_distance_from_center);
-            var magnitude = Math.sqrt(ball.dx * ball.dx + ball.dy * ball.dy);
-            ball.dx = Math.cos(reflected_angle) * magnitude;
-            ball.dy = Math.sin(reflected_angle) * magnitude;
-          }
-        }
-      }
-    }
-    if (lefty <= (paddle.y + paddle.height)) {
-      if (lefty >= paddle.y) {
-        if (leftx <= (paddle.x + paddle.width)) {
-          if (leftx >= paddle.x) {
-            ball.dx *= -1;
-          }
-        }
-      }
-    }
-    if (righty <= (paddle.y + paddle.height)) {
-      if (righty >= paddle.y) {
-        if (rightx <= (paddle.x + paddle.width)) {
-          if (rightx >= paddle.x) {
-            ball.dx *= -1;
-          }
-        }
-      }
-    }
+
+    // ball collides with paddle?
+    if (point_rect_collision(bottomx, bottomy, paddle))
+      ball.dy *= -1;
+    if (point_rect_collision(leftx, lefty, paddle))
+      ball.dx *= -1;
+    if (point_rect_collision(rightx, righty, paddle))
+      ball.dx *= -1;
+      
     // edge of game collision:
     // left side of the game
     if (ball.x - ball.radius <= 0) {
       ball.dx = ball.dx * -1;
     }
+
     // right side of the game
     if (ball.x + ball.radius >= ball.parent.width) {
       ball.dx = ball.dx * -1;
     }
+
     // top of the game
     if (ball.y - ball.radius <= 0) {
       ball.dy = ball.dy * -1;
     }
+
     // bottom of the game
     if (ball.y + ball.radius >= ball.parent.height) {
       // FIXME: lives -= 1
       ball.dy = ball.dy * -1;
     }
-    // brick collision
 
+    // brick collision
+    // For each brick
+    //  See if the top of the ball has collided
     for (i = 0; i < bricks.length; i ++) {
       brick = bricks[i];
-      if (topy <= (brick.y + brick.height)) {
-        if (topy >= brick.y) {
-          if (topx <= (brick.x + brick.width)) {
-            if (topx >= brick.x) {
-              console.log("top");
-              brick.remove();
-              bricks.splice(i, 1);
-              ball.dy = ball.dy * -1;
-              break;
-            }
-          }
-        }
+      if (point_rect_collision(topx, topy, brick)) {
+        brick.remove();
+        bricks.splice(i, 1);
+        ball.dy *= -1;
+        break;
       }
-      if (lefty <= (brick.y + brick.height)) {
-        if (lefty >= brick.y) {
-          if (leftx <= (brick.x + brick.width)) {
-            if (leftx >= brick.x) {
-              console.log("left");
-              brick.remove();
-              bricks.splice(i, 1);
-              ball.dx = ball.dx * -1;
-              break;
-            }
-          }
-        }
+      //  See if the bottom of the ball has colided
+      if (point_rect_collision(bottomx, bottomy, brick)) {
+        brick.remove();
+        bricks.splice(i, 1);
+        ball.dy *= -1;
+        break;
       }
-      if (righty <= (brick.y + brick.height)) {
-        if (righty >= brick.y) {
-          if (rightx <= (brick.x + brick.width)) {
-            if (rightx >= brick.x) {
-              console.log("right");
-              brick.remove();
-              bricks.splice(i, 1);
-              ball.dx = ball.dx * -1;
-              break;
-            }
-          }
-        }
+      //  See if the left of the ball has collided
+      if (point_rect_collision(leftx, lefty, brick)) {
+        brick.remove();
+        bricks.splice(i, 1);
+        ball.dx *= -1;
+        break;
       }
-      if (bottomy <= (brick.y + brick.height)) {
-        if (bottomy >= brick.y) {
-          if (bottomx <= (brick.x + brick.width)) {
-            if (bottomx >= brick.x) {
-              console.log("bottom");
-              brick.remove();
-              bricks.splice(i, 1);
-              ball.dy = ball.dy * -1;
-              break;
-            }
-          }
-        }
+      //  See if the right of the ball has collided
+      if (point_rect_collision(rightx, righty, brick)) {
+        brick.remove();
+        bricks.splice(i, 1);
+        ball.dx *= -1;
+        break;
       }
     }
+
+    // After the collision has or hasn't taken place
+    // Update the location of the ball.
     ball.x += ball.dx;
     ball.y += ball.dy;
-    if (paddleLeft) {
-      if (paddle.x > 0) {
-        paddle.x -= 5;
-      }
-    }
-    if (paddleRight) {
-      if (paddle.x + paddle.width < paddle.parent.width) {
-        paddle.x += 5;
-      }
-    }
   });
 })();
